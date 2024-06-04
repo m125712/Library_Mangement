ecbb1d261ca4429887484607d71b9542f801e937
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2024-06-04 15:20:21 +0600

    commit

--- Library_Mangement.py
+++ Library_Mangement.py
@@ -1,74 +1,101 @@
 class Book:
     def __init__(self,title:str,author_name:str,ISBN:str, price:float):
         assert  price >=0 ,"price should greater than 0"
-        self.title=title
-        self.author_name=author_name
-        self.ISBN=ISBN
-        self.price=price
+        self.__title=title
+        self.__author_name=author_name
+        self.__ISBN=ISBN
+        self.__price=price
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def author_name(self):
+        return self.__author_name
+
+    @property
+    def ISBN(self):
+        return self.__ISBN
+
+    @property
+    def price(self):
+        return self.__price
     def __repr__(self):
         return f"Book('{self.title}','{self.author_name}','{self.ISBN}',{self.price})"
     
 class Member:
     def __init__(self,ID:int,name:str,age:int,email:str):
         assert  age >=0 ,"age should greater than 0"
-        self.ID=ID
-        self.name=name
-        self.age=age
-        self.email=email
+        self.__ID=ID
+        self.__name=name
+        self.__age=age
+        self.__email=email
+    @property
+    def ID(self):
+        return self.__ID
+
+    @property
+    def age(self):
+        return self.__age
+
+    @property
+    def email(self):
+        return self.__email
+        return self.__price
     def __repr__(self):
         return f"Book({self.ID},'{self.name}','{self.age}','{self.email}')"
         
 class Library:
     def __init__(self):
-        self.books = {}
-        self.members = {}
-        self.borrowed_books = {}
+        self.__books = {}
+        self.__members = {}
+        self.__borrowed_books = {}
 
     def add_book(self, book: Book):
-        if book.ISBN in self.books:
+        if book.ISBN in self.__books:
             raise ValueError(f"A book with ISBN '{book.ISBN}' already exists in the library.")
-        self.books[book.ISBN] = book
+        self.__books[book.ISBN] = book
 
     def add_member(self, member: Member):
-        if member.ID in self.members:
+        if member.ID in self.__members:
             raise ValueError(f"A member with ID {member.ID} already exists in the library.")
-        self.members[member.ID] = member
+        self.__members[member.ID] = member
     def show_all_books(self):
 
-        if not self.books:
+        if not self.__books:
             print("There are currently no books in the library.")
         else:
             print("** Books in Library **")
-            for isbn, book in self.books.items():
+            for isbn, book in self.__books.items():
                 print(f"ISBN: {isbn}")
                 print(f"Title: {book.title}")
                 print(f"Author: {book.author_name}")
                 print(f"Price: ${book.price:.2f}") 
                 print("-" * 20) 
     def borrow_book(self, member_id: int, book_isbn: str):
-        if member_id not in self.members:
+        if member_id not in self.__members:
             raise ValueError(f"Member with ID {member_id} is not a member of the library.")
             
-        if book_isbn not in self.books:
+        if book_isbn not in self.__books:
             raise ValueError(f"Book with ISBN {book_isbn} is not available in the library.")
          
-        if book_isbn in self.borrowed_books:
+        if book_isbn in self.__borrowed_books:
             raise ValueError(f"Book with ISBN {book_isbn} is already borrowed by someone.")
             
-        self.borrowed_books[book_isbn] = member_id
+        self.__borrowed_books[book_isbn] = member_id
         print(f"Book with ISBN {book_isbn} is successfully borrowed by member with ID {member_id}.")
 
     def return_book(self, member_id: int, book_isbn: str):
-        if member_id not in self.members:
+        if member_id not in self.__members:
             raise ValueError(f"Member with ID {member_id} is not a member of the library.")
             return
-        if book_isbn not in self.borrowed_books:
+        if book_isbn not in self.__borrowed_books:
             raise ValueError(f"Book with ISBN {book_isbn} is not borrowed from the library.")
             return
-        if self.borrowed_books[book_isbn] != member_id:
+        if self.__borrowed_books[book_isbn] != member_id:
             raise ValueError(f"Book with ISBN {book_isbn} is not borrowed by member with ID {member_id}.")
             return
-        del self.borrowed_books[book_isbn]
+        del self.__borrowed_books[book_isbn]
         print(f"Book with ISBN {book_isbn} is successfully returned by member with ID {member_id}.")
 
 def main():
